aa62c1da418a1d5ba317849119a6f49bc830d27c
hasegawa_itsuki <ii440092@icloud.com>
2024-11-09 12:53:01 +0900

    cssの練習

--- alarm/app.py
+++ alarm/app.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
+from cron import cron
 
 app = Flask(__name__)
 
 
-# ブラウザに画面を表示
+# 関数名は全部違うものにする必要がある
+
 @app.route('/', methods=['GET'])
 def get():
     return render_template('index.html', alarm_time=f'')
 
 
-# ブラウザからのデータ取得
 @app.route('/', methods=['POST'])
 def index():
     alarm_time = request.form.get('alarm_time')
+    cron.set_job(alarm_time)
     return render_template('index.html', alarm_time=f'{alarm_time}にセットしました。')
 
+@app.route('/test')
+def test_index():
+    my_dict = {
+        'insert_something1': 'views.pyのinsert_something1部分です。',
+        'insert_something2': 'views.pyのinsert_something2部分です。',
+        'test_titles': ['title1', 'title2', 'title3']
+    }
+    return render_template('testapp/index.html', my_dict=my_dict)
 
 if __name__ == '__main__':
     app.run("0.0.0.0")
